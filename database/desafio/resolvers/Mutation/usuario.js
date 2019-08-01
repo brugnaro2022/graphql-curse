da7dcf3cfd39f873cbddf7cb433b6ae84292824e
marcos <marcosrodriguesbrugnaro@gmail.com>
2019-07-31 21:32:35 -0300

    Update mutation

--- database/desafio/resolvers/Mutation/usuario.js
+++ database/desafio/resolvers/Mutation/usuario.js
@@ -1,5 +1,6 @@
 const db = require('../../config/db')
-const { perfil: obterPerfil } = require('../Query/perfil');
+const { perfil: obterPerfil } = require('../Query/perfil')
+const { usuario: obterUsuario } = require('../Query/usuario')
 
 module.exports = {
   async novoUsuario(_, { dados }) {
@@ -7,7 +8,7 @@ module.exports = {
       const idsPerfis = []
 
       if (dados.perfis) {
-        for (filtro of dados.perfis) {
+        for (let filtro of dados.perfis) {
           const perfil = await obterPerfil(_, {
             filtro
           })
@@ -19,7 +20,7 @@ module.exports = {
       const [id] = await db('usuarios')
         .insert(dados)
 
-      for (perfil_id of idsPerfis) {
+      for (let perfil_id of idsPerfis) {
         await db('usuarios_perfis')
           .insert({ perfil_id, usuario_id: id })
       }
@@ -32,8 +33,18 @@ module.exports = {
       throw new Error(e.sqlMessage)
     }
   },
-  async excluirUsuario(_, { filtro }) {
+  async excluirUsuario(_, args) {
     try {
+      const usuario = await obterUsuario(_, args)
+      if (usuario) {
+        const { id } = usuario
+        await db('usuarios_perfis')
+          .where({ usuario_id: id }).delete()
+        await db('usuarios')
+          .where({ id }).delete()
+      }
+
+      return usuario
 
     } catch (e) {
       throw new Error(e.sqlMessage)
@@ -41,9 +52,40 @@ module.exports = {
   },
   async alterarUsuario(_, { filtro, dados }) {
     try {
+      const usuario = await obterUsuario(_, { filtro })
+      if (usuario) {
+        const { id } = usuario
+        if (dados.perfis) {
+          await db('usuarios_perfis')
+            .where({ usuario_id: id }).delete()
+
+          for (let filtro of dados.perfis) {
+            
+            const perfil = await obterPerfil(_, {
+              filtro
+            })
+
+            perfil && await db('usuarios_perfis')
+              .insert({
+                perfil_id: perfil.id,
+                usuario_id: id
+              })
+
+          }
+
+        }
+
+        delete dados.perfis
+        await db('usuarios')
+          .where({ id })
+          .update(dados)
+
+      }
+
+      return !usuario ? null : { ...usuario, ...dados }
 
     } catch (e) {
-      throw new Error(e.sqlMessage)
+      throw new Error(e)
     }
   }
 }
